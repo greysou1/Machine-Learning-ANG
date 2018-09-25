6c301dee0375846de131790637d11d12afe23367
GreySoul <prudvi.kamtam16@gmail.com>
2018-09-25 13:36:38 +0530

    Implemented Compute centroids

--- machine-learning-ex7/ex7/computeCentroids.m
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -25,13 +25,10 @@ centroids = zeros(K, n);
 %
 % Note: You can use a for-loop over the centroids to compute this.
 %
-
-
-
-
-
-
-
+for i = 1:K,
+    sel = find(idx == i);
+    centroids(i,:) = mean(X(sel,:));
+end
 
 % =============================================================
 
