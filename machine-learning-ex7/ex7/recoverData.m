1a8758de29ad595885348b3a8130ce77d1cc1731
GreySou1 <prudvi.kamtam16@gmail.com>
2019-08-24 10:33:38 +0530

    added ex7 and ex8

--- machine-learning-ex7/ex7/recoverData.m
+++ machine-learning-ex7/ex7/recoverData.m
@@ -19,9 +19,12 @@ X_rec = zeros(size(Z, 1), size(U, 1));
 %                    recovered_j = v' * U(j, 1:K)';
 %
 %               Notice that U(j, 1:K) is a row vector.
-%               
+%  
+
+v = Z';
 
 
+X_rec = v' * U(:,1:K)';
 
 % =============================================================
 
